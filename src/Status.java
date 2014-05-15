ccf2cab2dea7e41672806aebf354a6d3078c553d
albyrea <alberto_reali@hotmail.com>
2014-05-15 11:07:31 +0200

    Get con parametri
    WS rest con parametri

--- src/Status.java
+++ src/Status.java
@@ -9,4 +9,13 @@ public class Status {
      public String hello(){  
          return "Hello World!!! dineshonjava";      
      }  
+	 
+	 @GET
+	 @Path("/hello/{nome}/{cognome}")
+	 @Produces("text/plain")
+	 public String helloWithName(@PathParam("nome") String varX,
+	     @PathParam("cognome") String varY) {
+	         
+		 return "Benvenuto, " + varX + " " + varY;
+	 }
 }
